99928f34b9797bc5f7f1c7f1d97ff8f8259a3c63
Acong <handsd6111@gmail.com>
2022-08-14 13:04:37 +0000

    新增TDX授權的Model (未完成)

---
+++ miss-no-stop-api/app/Models/TDXAuth.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class TDXAuth
+{
+
+    private static function getAuthObject()
+    {
+    }
+
+    public static function hasTokenExpired()
+    {
+    }
+
+    public static function getAcessToken()
+    {
+    }
+
+    public static function setAuthObject($authObject)
+    {
+    }
+}

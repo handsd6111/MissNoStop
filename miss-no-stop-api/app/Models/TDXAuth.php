a6c95ffe73c696b3d475ce4d5c161f1e56b00ded
s20313116 <handsd6111@gmail.com>
2022-08-15 09:18:37 +0000

    TDX取得授權的Model

--- miss-no-stop-api/app/Models/TDXAuth.php
+++ miss-no-stop-api/app/Models/TDXAuth.php
@@ -2,24 +2,29 @@
 
 namespace App\Models;
 
+use App\Controllers\TDXAuthController;
 use CodeIgniter\Model;
+use Config\Services as CS;
+use DateTime;
 
 class TDXAuth
 {
-
-    private static function getAuthObject()
+    public static function getAuthObject()
     {
-    }
+        $TDXAuthObject = CS::cache()->get('TDXAuthObject');
 
-    public static function hasTokenExpired()
-    {
-    }
-
-    public static function getAcessToken()
-    {
+        if ($TDXAuthObject === NULL) {
+            TDXAuthController::getAndSetAuthObject();
+            return false;
+        } else {
+            return $TDXAuthObject;
+        }
     }
 
     public static function setAuthObject($authObject)
     {
+        $expires_in = $authObject->expires_in;
+        $authObject->expires_in = time() + $expires_in;
+        CS::cache()->save('TDXAuthObject', $authObject, $expires_in);
     }
 }

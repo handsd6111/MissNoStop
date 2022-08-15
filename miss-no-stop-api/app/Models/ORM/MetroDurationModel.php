72eecb99a67736420ccfffe4d99c9a83d7a74c32
s20313116 <handsd6111@gmail.com>
2022-08-15 09:16:27 +0000

    ORM Model

---
+++ miss-no-stop-api/app/Models/ORM/MetroDurationModel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models\ORM;
+
+use CodeIgniter\Model;
+
+class MetroDurationModel extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'metrodurations';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = false;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'MD_station_uid',
+        'MD_end_station_uid',
+        'MD_duration'
+    ];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}

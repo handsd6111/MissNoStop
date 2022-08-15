45d38df610cd3988a3b2749e252c6acfaf55d433
s20313116 <handsd6111@gmail.com>
2022-08-15 09:17:46 +0000

    主要Controller (未完成)

--- miss-no-stop-api/app/Controllers/TDXDataController.php
+++ miss-no-stop-api/app/Controllers/TDXDataController.php
@@ -2,76 +2,81 @@
 
 namespace App\Controllers;
 
+use App\Models\ORM\CityModel;
+use App\Models\ORM\MetroRouteModel;
 use App\Models\TDXAuth;
-use CodeIgniter\Controller;
-use Config\CURLRequest;
 use Exception;
-use CodeIgniter\Model;
 use \Config\Services as CS;
 
-class TDXDataController extends BaseController
+class TDXDataController extends TDXBaseController
 {
-    public function getCities()
+
+
+    /**
+     * 
+     */
+    public function getAndSetCities()
     {
-        $url = "https://api.nlsc.gov.tw/other/ListCounty";
+        $accessToken = $this->getAccessToken();
+        $url = "https://tdx.transportdata.tw/api/basic/v2/Basic/City?%24format=JSON";
+        $result = $this->curlGet($url, $accessToken);
 
-        $client = CS::curlrequest();
-        $response = $client->request(
-            'GET',
-            $url
-        );
+        foreach ($result as $value) {
 
-        $nonCodingXML = $response->getXML(); // response取得的是未編碼前的XML
-        $realXML = simplexml_load_string($nonCodingXML)->item0; // 將未編碼的XML轉成正常的XML
-        return simplexml_load_string($realXML)->countyItem; //最後將XML轉成物件並回傳
+            $saveData = [
+                'C_id' => $value->CityCode,
+                'C_name_TC' => $value->CityName,
+                'C_name_EN' => $value->City
+            ];
+
+            $cityModel = new CityModel();
+            $cityModel->save($saveData); //orm save data
+        }
+
+        return true;
     }
 
-    public function getAuthObject()
+    public function getAndSetMetroRoute($railSystem, $countyCode)
     {
-        $url = "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token";
-
-        $postData = array(
-            "grant_type" => "client_credentials",
-            "client_id" => "s20313116-16db10ec-5006-4f44",
-            "client_secret" => "2ead03cf-908c-4103-a2df-38afe764314f"
-        );
-
-        $client = CS::curlrequest();
-        $response = $client->request(
-            'POST',
-            $url,
-            [
-                'headers' => [
-                    'content-type' => 'application/x-www-form-urlencoded'
-                ],
-                'form_params' => $postData
-            ]
-        );
-        $result = json_decode(json_decode($response->getJSON()));
-        TDXAuth::setAuthObject($result);
-        return TDXAuth::getAcessToken();
-    }
-
-    public function getMetroStation()
-    {
-        // $accessToken = "";
-        $accessToken = $this->getAuthObject();
+        $accessToken = $this->getAccessToken();
         // $url = "https://tdx.transportdata.tw/api/basic/v2/Rail/Metro/StationTimeTable/TRTC?format=JSON";
-        $url = "https://tdx.transportdata.tw/api/basic/v2/Rail/Metro/StationTimeTable/KRTC?%24top=30&%24format=JSON";
+        $url = "https://tdx.transportdata.tw/api/basic/v2/Rail/Metro/Line/" . $railSystem . "?%24format=JSON";
 
-        $ch = curl_init();
+        $result = $this->curlGet($url, $accessToken);
+        // var_dump($result);
+        foreach ($result as $value) {
 
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $saveData = [
+                'MR_id' => $countyCode . '-' . $value->LineNo,
+                'MR_name_TC' => isset($value->LineName->Zh_tw) ? $value->LineName->Zh_tw : "",
+                'MR_name_EN' => isset($value->LineName->En) ? $value->LineName->En : "",
+            ];
+            // var_dump($saveData);
+            $metroRouteModel = new MetroRouteModel();
+            $metroRouteModel->save($saveData); //orm save data
+        }
+    }
 
-        $headers = [
-            'accept: application/json',
-            'authorization: Bearer ' . $accessToken,
-        ];
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        $output = curl_exec($ch);
-        curl_close($ch);
 
-        return $output;
+    public function getAndSetMetroStation($railSystem, $countyCode)
+    {
+        $accessToken = $this->getAccessToken();
+        // $url = "https://tdx.transportdata.tw/api/basic/v2/Rail/Metro/StationTimeTable/TRTC?format=JSON";
+        $url = "https://tdx.transportdata.tw/api/basic/v2/Rail/Metro/Line/" . $railSystem . "?%24format=JSON";
+
+        $result = $this->curlGet($url, $accessToken);
+
+        foreach ($result as $value) {
+
+            $saveData = [
+                'MS_id' => $value->CountyCode,
+                'C_name_TC' => $value->CountyName,
+                'C_name_EN' => $value->County,
+                'MS_city_id' => $countyCode,
+            ];
+
+            $cityModel = new CityModel();
+            $cityModel->save($saveData); //orm save data
+        }
     }
 }
